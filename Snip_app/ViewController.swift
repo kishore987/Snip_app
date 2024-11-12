0737cec48a4bb26de56a7231469897267b99db0f
AVK <Vamshi-kishore>
2024-11-12 06:24:42 -0800

    Initial Commit

---
+++ Snip_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snip_app
+//
+//  Created by Vamshi Kishore Aemula on 11/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
